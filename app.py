9a028a93ba2736333f7f76b5e73caf1fb791626a
kietn20 <kietnguyen3698@gmail.com>
2023-05-30 15:48:31 -0700

    integrated recommendations for the top-songs page with HTMX

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ auth_query_parameters = {
     'redirect_uri': REDIRECT_URI,
     'state': 'state',
     'scope': SCOPE,
-    'show_dialog': 'true'
+    'show_dialog': 'false'
 }
 
 access_token = [None, None]
@@ -54,14 +54,14 @@ def grantAccessToken():
         'redirect_uri': REDIRECT_URI
     }
     response = requests.post(SPOTIFY_TOKEN_URL, headers=headers, data=data)
-
+    print('get access token status code:', response.status_code)
     if response.status_code == 200:
         json_result = json.loads(response.content)
-        print(json_result)
+        # print(json_result)
         access_token[0] = json_result['access_token']
         access_token[1] = json_result['refresh_token']
-        # print('access_token:', access_token[0])
-        print("freshToken:", access_token[1])
+        print('access_token:', access_token[0])
+        # print("freshToken:", access_token[1])
         return redirect('/home')
     else:
         return response.content
@@ -76,6 +76,7 @@ def getTopSongs(limit=10, timeRange='short_term'):
     print('method:', request.method)
     print('access_token:',access_token[0])
     if request.method == 'POST':
+        print('option1: ', request.form.get('option1'))
         limit = int(request.form.get('option1'))
         timeRange = request.form.get('option2')
         API_URL = "https://api.spotify.com/v1/me/top/tracks"
@@ -90,18 +91,66 @@ def getTopSongs(limit=10, timeRange='short_term'):
                 "limit": limit
             })
         json_resp = response.json()
-        print('response code:', response.status_code)
+        # print(json_resp)
+        print('getTopSongs response code:', response.status_code)
         if response.status_code == 200:
-            topSongs = []
+            topSongsID = []
             for songs in range(limit):
-                topSongs.append( (json_resp['items'][songs]['name'], json_resp['items'][songs]['artists'][0]['name'], json_resp['items'][songs]['id']) )
-            numberOfSongs = len(topSongs)
-            return render_template('songs.html', length=numberOfSongs, topSongs=topSongs, showRecommendations=True)
+                topSongsID.append(json_resp['items'][songs]['id'])
+            numberOfSongs = len(topSongsID)
+
+            recommendations = ','.join(getRecommendations(limit, seed_tracks=topSongsID))
+
+            return render_template('songs.html', length=numberOfSongs, topSongs=topSongsID, showRecommendations=True, recommendations=recommendations)
         else:
             refreshAccessToken()
             return redirect('/top-songs')
     elif request.method == 'GET':
         return render_template('songs.html', length=0, topSongs=[], showRecommendations=False)
+    
+@app.route('/getRecommendations', methods=['GET'])
+def getRecommendations(limit, seed_tracks=[], seed_genres=[], seed_artists=[]):
+    API_URL = 'https://api.spotify.com/v1/recommendations'
+    response = requests.get(
+        API_URL, 
+        headers={
+            "Authorization": f"Bearer {access_token[0]}",
+            "Content-Type": "application/json"
+        },
+        params={
+            "limit": limit,
+            "market": 'NA',
+            "seed_tracks": seed_tracks,
+            "seed_genres": seed_genres,
+            "seed_artists": seed_artists
+        })
+    json_resp = response.json()
+
+    if response.status_code == 200:
+        recommendations = []
+        for song in range(limit):
+            recommendations.append(json_resp['tracks'][song]['id'])
+        return recommendations
+    else:
+        return json_resp
+    
+@app.route('/clicked', methods=['GET'])
+def showTrackRecommendations():
+    # print("--------------route /click")
+    length = int(request.values.get('myVal'))
+    recommendations = request.values.get('a')
+    # print('--------len:', length, type(length))
+    # print('--------recommendations:', recommendations, type(recommendations))
+    recommendations = recommendations.split(',')
+    # print('--------len:', length, type(length))
+    # print('--------recommendations 2:', recommendations, type(recommendations))
+    return render_template('track_recommendations.html', length=length, recommendations=recommendations)
+
+@app.route('/show-recommendations', methods=['GET'])
+def showRecommendations():
+    pass
+
+
     
 @app.route('/refresh-access-token', methods=['POST'])
 def refreshAccessToken():
