49d2c0620746892e57bc02536a2085f99349cd6d
kietn20 <kietnguyen3698@gmail.com>
2023-05-29 22:20:25 -0700

    Implemented POST request getTopSongs and display them on the webpage, styled backgrounds, changed routes to avoid errors, stored fresh tokens, and implemented home page

--- app.py
+++ app.py
@@ -27,10 +27,10 @@ auth_query_parameters = {
     'redirect_uri': REDIRECT_URI,
     'state': 'state',
     'scope': SCOPE,
-    'show_dialog': 'false'
+    'show_dialog': 'true'
 }
 
-access_token = [None]
+access_token = [None, None]
 # auth_code = ''
 
 @app.route("/")
@@ -38,7 +38,7 @@ def login():
     return redirect(SPOTIFY_AUTH_URL + urlencode(auth_query_parameters))
 
 @app.route('/callback/', methods=['GET'])
-def callback():
+def grantAccessToken():
     auth_code = request.args['code']
     # print('code:', request.args['code'])
     auth_string = CLIENT_ID + ':' + CLIENT_SECRET
@@ -55,24 +55,29 @@ def callback():
     }
     response = requests.post(SPOTIFY_TOKEN_URL, headers=headers, data=data)
 
-    if response.status_code != 400:
+    if response.status_code == 200:
         json_result = json.loads(response.content)
-        access_token[0] = (json_result['access_token'])
+        print(json_result)
+        access_token[0] = json_result['access_token']
+        access_token[1] = json_result['refresh_token']
         # print('access_token:', access_token[0])
-        return render_template('home.html')
+        print("freshToken:", access_token[1])
+        return redirect('/home')
     else:
         return response.content
+    
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
-@app.route('/top-songs', methods=["POST"])
-def getTopSongs(limit=10, offset=0, timeRange='short_term'):
+@app.route('/top-songs', methods=["POST", "GET"])
+def getTopSongs(limit=10, timeRange='short_term'):
     print('route: /top-songs')
     print('method:', request.method)
     print('access_token:',access_token[0])
     if request.method == 'POST':
         limit = int(request.form.get('option1'))
         timeRange = request.form.get('option2')
-        # print('limit:', limit, 'type:', type(limit))
-        # print('timeRange:', timeRange)
         API_URL = "https://api.spotify.com/v1/me/top/tracks"
         response = requests.get(
             API_URL,
@@ -91,9 +96,34 @@ def getTopSongs(limit=10, offset=0, timeRange='short_term'):
             for songs in range(limit):
                 topSongs.append( (json_resp['items'][songs]['name'], json_resp['items'][songs]['artists'][0]['name'], json_resp['items'][songs]['id']) )
             numberOfSongs = len(topSongs)
-            return render_template('songs.html', length=numberOfSongs, topSongs=topSongs)
+            return render_template('songs.html', length=numberOfSongs, topSongs=topSongs, showRecommendations=True)
         else:
-            return json_resp
+            refreshAccessToken()
+            return redirect('/top-songs')
+    elif request.method == 'GET':
+        return render_template('songs.html', length=0, topSongs=[], showRecommendations=False)
+    
+@app.route('/refresh-access-token', methods=['POST'])
+def refreshAccessToken():
+    refresh_token = access_token[1]
+    auth_string = CLIENT_ID + ':' + CLIENT_SECRET
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+    headers = {
+        'Authorization': 'Basic ' + auth_base64,
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data = {
+        'grant_type': 'refresh_token',
+        'refresh_token': refresh_token
+    }
+    response = requests.post(SPOTIFY_TOKEN_URL, headers=headers, data=data)
+
+    if response.status_code == 200:
+        json_result = json.loads(response.content)
+        access_token[0] = json_result['access_token']
+    else:
+        return response.content
 
 if __name__ == "__main__":
     app.run(debug=True, port=PORT)
